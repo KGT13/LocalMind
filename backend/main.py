faa534ae3b5f5a772282a3b4a5f4eea59ca7bf5d
KGT13 <karellk1311@gmail.com>
2026-07-12 11:42:14 -0400

    Update backend entrypoint for packaged Electron execution

--- backend/main.py
+++ backend/main.py
@@ -349,4 +349,6 @@ async def save_note(request: NoteRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import multiprocessing
+    multiprocessing.freeze_support()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
